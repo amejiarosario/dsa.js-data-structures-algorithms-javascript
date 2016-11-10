351c8e9d2d64220a31d0a2d07d8ec49938ae10da
Adrian Mejia <admejiar@cisco.com>
2016-11-09 19:01:54 -0500

    more checks

--- one-away.js
+++ one-away.js
@@ -2,13 +2,19 @@ function isOneEditAway(str1, str2) {
   let edit = 0;
   let i1, i2;
 
+  if(Math.abs(str1.length - str2.length) > 1) {
+    return false;
+  }
+
   for(i1 = 0, i2 = 0; i1 < str1.length && i2 < str2.length && edit <= 1; i1++, i2++) {
     if(str1[i1] === str2[i2]) {
       continue;
     } else {
       edit++;
 
-      if(str1.length > str2.length) {
+      if(edit > 1){
+        return false;
+      } else if(str1.length > str2.length) {
         i2--;
       } else if(str1.length < str2.length) {
         i1--;
@@ -28,6 +34,8 @@ console.log(isOneEditAway('pale', 'pale')); // true
 console.log(isOneEditAway('pale', 'ale')); // true
 console.log(isOneEditAway('pale', 'pales')); // true
 console.log(isOneEditAway('pale', 'bale')); // true
+console.log(isOneEditAway('apple', 'aple')); // true
+
 console.log(isOneEditAway('pale', 'bae')); // false
 console.log(isOneEditAway('pale', 'elap')); // false
 console.log(isOneEditAway('pale', 'palepalepale')); // false
