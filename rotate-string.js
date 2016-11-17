e2abb008c42487158859fa8ba92ce34a86fd501b
Adrian Mejia <admejiar@cisco.com>
2016-11-17 16:49:33 -0500

    better implementation after reading answer

--- rotate-string.js
+++ rotate-string.js
@@ -1,4 +1,32 @@
+// O (n)
 function isStringRotation(s1, s2) {
+  return isSubstring(s1 + s1, s2);
+}
+
+function isSubstring(s1, s2) {
+  return s1.indexOf(s2) > -1 || s2.indexOf(s1) > -1;
+}
+
+console.log(isStringRotation('ab', 'ab'));
+console.log(isStringRotation('ba', 'ab'));
+
+console.log(isStringRotation('aab', 'aab'));
+console.log(isStringRotation('aba', 'aab'));
+console.log(isStringRotation('baa', 'aab'));
+
+
+console.log(isStringRotation('aaaab', 'aaaab'));
+console.log(isStringRotation('aaaba', 'aaaab'));
+console.log(isStringRotation('aabaa', 'aaaab'));
+console.log(isStringRotation('abaaa', 'aaaab'));
+console.log(isStringRotation('baaaa', 'aaaab'));
+
+console.log(isStringRotation('waterbottle', 'bottlewater'));
+
+console.log(isStringRotation('waterbotlle', 'bottlewater')); // false
+
+// O (n^2)
+function isStringRotationSlow(s1, s2) {
   if(s1.length !== s2.length) {
     return false;
   }
@@ -22,23 +50,3 @@ function isStringRotation(s1, s2) {
   }
   return false;
 }
-
-function isSubstring(s1, s2) {
-  return s1.indexOf(s2) > -1 || s2.indexOf(s1) > -1;
-}
-
-console.log(isStringRotation('ab', 'ab'));
-console.log(isStringRotation('ba', 'ab'));
-
-console.log(isStringRotation('aab', 'aab'));
-console.log(isStringRotation('aba', 'aab'));
-console.log(isStringRotation('baa', 'aab'));
-
-
-console.log(isStringRotation('aaaab', 'aaaab'));
-console.log(isStringRotation('aaaba', 'aaaab'));
-console.log(isStringRotation('aabaa', 'aaaab'));
-console.log(isStringRotation('abaaa', 'aaaab'));
-console.log(isStringRotation('baaaa', 'aaaab'));
-
-console.log(isStringRotation('waterbottle', 'bottlewater'));
