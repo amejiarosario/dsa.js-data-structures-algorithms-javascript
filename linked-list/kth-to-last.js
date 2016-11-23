3ebe21881ce319e38f13b7fcc27efda79e845821
Adrian Mejia <admejiar@cisco.com>
2016-11-23 11:43:59 -0500

    multiple solutions

--- linked-list/kth-to-last.js
+++ linked-list/kth-to-last.js
@@ -1,17 +1,9 @@
 const LinkedList = require('./linkedlist');
 
-LinkedList.prototype.findKthToLast = function(k) {
-  let length = 0;
-  for(let n = this.head; n; n = n.next) { length++; }
 
-  if(k < 1 || k > length) {
-    return null;
-  }
-
-  let node = this.head;
-  for(let i = 0; i < length - k; i++) { node = node.next; }
-  return node.data;
-};
+LinkedList.prototype.findKthToLast = optimal;
+// LinkedList.prototype.findKthToLast = mySolution;
+// LinkedList.prototype.findKthToLast = recursive;
 
 let list = new LinkedList();
 list.add(4);
@@ -21,9 +13,81 @@ list.add(1);
 
 console.log(list.toString()); //
 
-console.log(list.findKthToLast(0)); // null
+console.log(list.findKthToLast(0)); // undefined
 console.log(list.findKthToLast(1)); // 4
 console.log(list.findKthToLast(2)); // 3
 console.log(list.findKthToLast(3)); // 2
 console.log(list.findKthToLast(4)); // 1
-console.log(list.findKthToLast(5)); // null
+console.log(list.findKthToLast(5)); // undefined
+
+
+// -----------------------
+
+/**
+ *
+ * @param k
+ * @returns {*}
+ */
+function optimal(k) {
+  let kth;
+  let last = this.head;
+
+  // move ahead "last" k-spaces ahead
+  for(let i = 0; i < k; i++) {
+    if(!last) { return; }
+    last = last.next;
+  }
+
+  // move both together
+  for(kth = this.head; last; last = last.next, kth = kth.next) { }
+  return kth && kth.data;
+}
+
+/**
+ *
+ *
+ * O (n) and O(n) space
+ * @param k
+ * @param n
+ * @param i
+ * @returns {*}
+ */
+function recursive(k, n = this.head, i = 0) {
+  if(n) {
+    let res = recursive(k, n.next, i+1);
+
+    if(res.index === i) {
+      res.node = n.data;
+    }
+
+    if(i === 0) {
+      return res.node;
+    } else {
+      return res;
+    }
+  } else {
+    return {node: undefined, index: i - k};
+  }
+}
+
+
+/**
+ * Do two rounds one to count and other one to return the kth value from last
+ *
+ * O(n)
+ *
+ * @param k
+ * @returns {*}
+ */
+function mySolution(k) {
+  let length = 0;
+  for(let n = this.head; n; n = n.next) { length++; }
+
+  if(k < 1 || k > length) {
+    return;
+  }
+
+  let node = this.head;
+  for(let i = 0; i < length - k; i++) { node = node.next; }
+  return node.data;
+};
