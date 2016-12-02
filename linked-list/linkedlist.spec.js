8945f2f3e5ed1f086bd4aabe8d35d1d076845e1c
Adrian Mejia <admejiar@cisco.com>
2016-12-02 18:37:55 -0500

    tests file

---
+++ linked-list/linkedlist.spec.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const LinkedList = require('./linkedlist');
+
+describe('LinkedList', function () {
+  describe('.addLast', function () {
+    it('should add elements to the tail', function () {
+      const list = new LinkedList();
+      list.add(1);
+      list.add(2);
+      list.add(3);
+      list.add(4);
+
+      assert(list.toString(), '1 -> 2 -> 3 -> 4');
+    });
+  });
+});
