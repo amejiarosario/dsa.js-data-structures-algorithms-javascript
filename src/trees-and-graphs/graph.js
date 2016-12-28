1d15ae9fa93ad2c9397afba7b945ee3f204db219
Adrian Mejia <admejiar@cisco.com>
2016-12-28 15:21:25 -0400

    get nodes

--- src/trees-and-graphs/graph.js
+++ src/trees-and-graphs/graph.js
@@ -1,54 +1,91 @@
 const Queue = require('../stacks-and-queues/queue');
+
 /**
- * Digraph
+ * Node with data and reference to adjacent nodes
  */
-class Graph {
-  constructor() {
-    this.nodes = {};
-  }
-
-  /**
-   * use adjacency list to store nodes and more
-   *
-   * O(1)
-   *
-   * @param a
-   * @param b
-   */
-  add(a, b) {
-    if(!this.nodes[a]) {
-      this.nodes[a] = [];
-    }
-
-    this.nodes[a].push(b);
-  }
-
-  /**
-   * Depth-first search
-   * @param node
-   */
-  dfs(node) {
-
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.adjacents = [];
   }
 }
 
 /**
- * Breadth-first search
- * @param node
+ * Directed Graph
+ * Note: no data duplicates are allowed
  */
-Graph.prototype.bfs = function* (node) {
+class Graph {
+  /**
+   * Initialize adjacency list
+   */
+  constructor() {
+    this.nodes = new Map();
+  }
+
+  /**
+   * Add a relationship between two nodes
+   *
+   * O(1)
+   *
+   * @param source node reference or node data
+   * @param target node reference or node data
+   * @returns {*} resulting source node
+   */
+  add(source, target) {
+    let sourceNode, targetNode;
+
+    sourceNode = this.getNode(source);
+    targetNode = this.getNode(target);
+
+    sourceNode.adjacents.push(targetNode);
+    this.nodes.set(sourceNode.data, sourceNode);
+
+    return sourceNode;
+  }
+
+  /**
+   * Get existing node or create a new one
+   * @param dataOrNode
+   * @returns {*}
+   */
+  getNode(dataOrNode) {
+    let node;
+
+    if(isNode(dataOrNode)) {
+      node = dataOrNode;
+    } else if(this.nodes.has(dataOrNode)) {
+      node = this.nodes.get(dataOrNode);
+    } else {
+      node = new Node(dataOrNode);
+    }
+    return node;
+  }
+}
+
+function isNode(thing){
+  return thing instanceof Node;
+}
+
+
+Graph.prototype.bfs = bfs;
+
+/**
+ * Breadth-first search
+ * @param start
+ */
+function* bfs(start) {
   const queue = new Queue();
 
-  queue.add(node);
+  queue.add(start);
 
   while(!queue.isEmpty()) {
-    const current = queue.remove();
-    yield current;
-    const adjacents = this.nodes[current] || [];
-    adjacents.forEach(function (adjNode) {
-      queue.add(adjNode);
-    });
+    const node = queue.remove();
+    yield node;
   }
-};
+}
 
-module.exports = Graph;
+
+module.exports = {
+  Graph,
+  Node
+};
--- src/trees-and-graphs/graph.spec.js
+++ src/trees-and-graphs/graph.spec.js
@@ -1,23 +1,24 @@
 const expect = require('chai').expect;
-const Graph = require('./graph');
+const Graph = require('./graph').Graph;
+const Node = require('./graph').Node;
 
 describe('Graph', function () {
   let graph;
 
-  beforeEach(function () {
+  beforeEach(function () { 
     graph = new Graph();
   });
 
   describe('add', function () {
     it('should add two elements', function () {
       graph.add(0, 1);
-      expect(graph.nodes[0]).to.eql([1]);
+
     });
 
     it('should allow one element to have two children', function () {
       graph.add(0, 1);
       graph.add(0, 2);
-      expect(graph.nodes[0]).to.eql([1, 2]);
+      expect(graph.nodes.get(0).adjacents[1].data).to.eql(2);
     });
 
     it('should store complex data', function () {
@@ -25,7 +26,47 @@ describe('Graph', function () {
       let b = {b:2};
 
       graph.add(a, b);
-      expect(graph.nodes[a]).to.eql([b]);
+      expect(graph.nodes.get(a).adjacents[0].data).to.eql(b);
+    });
+
+    it('should store nodes by reference and keep adjacents', function () {
+      const node1 = new Node(1);
+      const node2 = new Node(2);
+      const node3 = new Node(3);
+
+      node1.adjacents.push(node2);
+
+      graph.add(node1, node3);
+
+      expect(graph.nodes.get(node1.data).adjacents).to.eql([node2, node3]);
+    });
+
+    it('allows you to shot yourself in the shoe with duplicated adjacents', function () {
+      graph.add(0, 1);
+      graph.add(0, 1);
+      expect(graph.nodes.get(0).adjacents[0].data).to.eql(1);
+      expect(graph.nodes.get(0).adjacents[1].data).to.eql(1);
+      expect(graph.nodes.get(0).adjacents[2]).to.equal(undefined);
+    });
+  });
+
+  describe('getNode', function () {
+    it('creates new node with new data', function () {
+      const node = graph.getNode(1);
+      expect(node.data).to.equal(1);
+    });
+
+    it('returns existing node with data', function () {
+      const node1 = graph.add(1, 2);
+      const node = graph.getNode(1);
+      expect(node1).to.equal(node);
+    });
+
+    it('returns reference', function () {
+      const node1 = new Node(1);
+      const node2 = new Node(2);
+      graph.add(node1, node2);
+      expect(graph.getNode(node1)).to.equal(graph.nodes.get(node1.data));
     });
   });
 
