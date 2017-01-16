bc1459f2ace6e29a8b0819fbe471bc95c2da6b1c
Adrian Mejia <admejiar@cisco.com>
2017-01-16 13:24:03 -0500

    4.3 list of depths better solution

--- src/trees-and-graphs/list-of-depths.js
+++ src/trees-and-graphs/list-of-depths.js
@@ -1,7 +1,35 @@
-const Queue = require('../stacks-and-queues/queue');
 const LinkedList = require('../linked-list/linkedlist');
 
 function listOfDepths(root) {
+  const result = [];
+
+  if(!root) return result;
+
+  let current = new LinkedList();
+
+  current.addLast(root);
+
+  while(current.size()) {
+    result.push(current);
+
+    const parents = current;
+    current = new LinkedList();
+
+    for(let parent = parents.head; parent; parent = parent.next) {
+      parent.data.adjacents.forEach(function (adj) {
+        current.addLast(adj);
+      });
+    }
+  }
+
+  return result;
+}
+
+// ------- Alternative solution
+
+const Queue = require('../stacks-and-queues/queue');
+
+function listOfDepths2(root) {
   const queue = new Queue();
   const list = [];
   let depth = 0;
