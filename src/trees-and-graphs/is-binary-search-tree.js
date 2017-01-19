c2eecbc430e21c19adf9a29806c0f515cf7741ee
Adrian Mejia <admejiar@cisco.com>
2017-01-19 18:32:20 -0500

    4.5 is a binary search tree.js - shorter solution

--- src/trees-and-graphs/is-binary-search-tree.js
+++ src/trees-and-graphs/is-binary-search-tree.js
@@ -6,30 +6,9 @@
 function isBinarySearchTree(node, min, max) {
   if(!node) return true;
 
-  const parent = node.data;
-  let isBST = true, left, right;
-
-  if(node.left) {
-    left = node.left.data;
-    isBST = isBST && left <= parent;
-
-    if(min) {
-      isBST = isBST && left > min;
-    }
-  }
-
-  if(node.right) {
-    right = node.right.data;
-    isBST = isBST && right > parent;
-
-    if(max) {
-      isBST = isBST && right < max;
-    }
-  }
-
-  return isBST &&
-    isBinarySearchTree(node.left, null, parent) &&
-    isBinarySearchTree(node.right, parent, null);
+  return !((min && node.data <= min) || (max && node.data > max)) &&
+    isBinarySearchTree(node.left, min, node.data) &&
+    isBinarySearchTree(node.right, node.data, max);
 }
 
 module.exports = isBinarySearchTree;
