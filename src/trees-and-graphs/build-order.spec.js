115e44eea7733013f45c3bccf3650294d4bda033
Adrian Mejia <admejiar@cisco.com>
2017-01-24 15:10:48 -0500

    4.7 Build order - my first solution, some test cases failing

---
+++ src/trees-and-graphs/build-order.spec.js
@@ -0,0 +1,30 @@
+const expect = require('chai').expect;
+const Graph = require('./graph').Graph;
+const getBuildOrder = require('./build-order');
+
+describe('Graph: build order', function () {
+
+  it('should find the right order', function () {
+    const projects = ['a', 'b', 'c', 'd', 'e', 'f'];
+    const dependencies = [['a', 'd'], ['f', 'b'], ['b', 'd'], ['f', 'a'], ['d', 'c']];
+    expect(getBuildOrder(projects, dependencies).join()).to.equal('e,f,b,a,d,c');
+  });
+
+  it('should find the right order 2', function () {
+    const projects = ['a', 'b', 'c', 'd'];
+    const dependencies = [['a', 'b'], ['c', 'd']];
+    expect(getBuildOrder(projects, dependencies).join()).to.equal('a,b,c,d');
+  });
+
+  it('should work where a node links to a node that cannot immediately follow it', function () {
+    const projects = ['a', 'b', 'c', 'f'];
+    const dependencies = [['f', 'a'], ['f', 'c'], ['f', 'b'], ['c', 'a'], ['b', 'a']];
+    expect(getBuildOrder(projects, dependencies).join()).to.equal('f,c,b,a');
+  });
+
+  it('should work with complex graph', function () {
+    const projects = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+    const dependencies = [['f', 'c'], ['f', 'b'], ['f', 'a'], ['c', 'a'], ['b', 'a'], ['a', 'e'], ['b', 'e'], ['d', 'g']];
+    expect(getBuildOrder(projects, dependencies).join()).to.equal('f,c,b,a');
+  });
+});
