81ac87386eae4039bb9cf82f32f543d4899f946e
Adrian Mejia <admejiar@cisco.com>
2017-01-25 16:23:11 -0500

    better tests

--- src/trees-and-graphs/build-order.spec.js
+++ src/trees-and-graphs/build-order.spec.js
@@ -4,6 +4,14 @@ const getBuildOrder = require('./build-order');
 
 describe('Graph: build order', function () {
 
+  it('should work where a node links to a node that cannot immediately follow it', function () {
+    const projects = ['a', 'b', 'c', 'f'];
+    const dependencies = [['f', 'c'], ['f', 'a'], ['f', 'b'], ['c', 'a'], ['b', 'a']];
+    expect(getBuildOrder(projects, dependencies).join('')).to.match(/^f(?=.*c)(?=.*b).*a$/);
+    // https://regex101.com/r/xI9qT0/6
+    // http://www.rubular.com/r/yf9y5NEi99
+  });
+
   it('should find the right order', function () {
     const projects = ['a', 'b', 'c', 'd', 'e', 'f'];
     const dependencies = [['a', 'd'], ['f', 'b'], ['b', 'd'], ['f', 'a'], ['d', 'c']];
@@ -14,12 +22,6 @@ describe('Graph: build order', function () {
     const projects = ['a', 'b', 'c', 'd'];
     const dependencies = [['a', 'b'], ['c', 'd']];
     expect(getBuildOrder(projects, dependencies).join()).to.equal('a,b,c,d');
-  });
-
-  it('should work where a node links to a node that cannot immediately follow it', function () {
-    const projects = ['a', 'b', 'c', 'f'];
-    const dependencies = [['f', 'a'], ['f', 'c'], ['f', 'b'], ['c', 'a'], ['b', 'a']];
-    expect(getBuildOrder(projects, dependencies).join()).to.equal('f,c,b,a');
   });
 
   it('should work with complex graph', function () {
