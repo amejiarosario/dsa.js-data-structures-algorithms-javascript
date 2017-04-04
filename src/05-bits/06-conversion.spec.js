7ea5eb5e2857009cba1740cfd35950af62341e6d
Adrian Mejia <admejiar@cisco.com>
2017-04-04 18:39:13 -0400

    5.6 solution

--- src/05-bits/06-conversion.spec.js
+++ src/05-bits/06-conversion.spec.js
@@ -2,11 +2,11 @@ const expect = require('chai').expect;
 const conversion = require('./06-conversion');
 
 describe('Bit Manipulation: conversion', function() {
-  it('power of two', function() {
-    expect(conversion(0b0)).to.equal(true);
+  it('29 and 15', function() {
+    expect(conversion(0b111101, 0b101111)).to.equal(2);
   });
 
-  it('no power of two', function() {
-    expect(conversion(0b111)).to.equal(false);
+  it('one and zero', function() {
+    expect(conversion(0b01, 0b00)).to.equal(1);
   });
 });
