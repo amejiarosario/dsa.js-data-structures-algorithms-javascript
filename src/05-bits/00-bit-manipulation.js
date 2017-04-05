5bff233df180d6ec7089c26c3236ef105e39e059
Adrian Mejia <admejiar@cisco.com>
2017-04-05 07:41:23 -0400

    5.0 update

--- src/05-bits/00-bit-manipulation.js
+++ src/05-bits/00-bit-manipulation.js
@@ -29,13 +29,27 @@ function clearBit(n, i) {
 }
 
 /**
- * Set bit to either 1 or 0
+ * Set/clear bit at a given index
  * @param n number
  * @param i index
  * @param b bit
  * @returns {number} new number with a given bit set/cleared
  */
 function updateBit(n, i, b) {
+  return (b > 0) ? setBit(n, i) : clearBit(n, i);
+}
+
+/**
+ * Set bit to either 1 or 0
+ *
+ * It first clear the bit with a mask and then set it to whatever b is.
+ *
+ * @param n number
+ * @param i index
+ * @param b bit either 0 or 1. If bigger than 1 value will overflow
+ * @returns {number} new number with a given bit set/cleared
+ */
+function updateBit2(n, i, b) {
   const mask = ~(1 << i);
   return (n & mask) | (b << i);
 }
