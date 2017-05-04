da5d2e4ade4d4df2e2e8c90c761afb134656eeb9
Adrian Mejia <admejiar@cisco.com>
2017-05-04 09:44:11 -0400

    node-version

--- src/07-object-oriented-design/othello.js
+++ src/07-object-oriented-design/othello.js
@@ -24,20 +24,73 @@ class Board {
     for(let y = 0; y < size; y++) {
       const row = [];
       for(let x = 0; x < size; x++) {
-        row.push(new Square());
+        row.push(new Square(x, y));
       }
       this.board.push(row);
     }
   }
 
   move(color, x, y) {
-    const square = this.board[y][x];
+    const square = this.get(x, y);
 
-    if(square.isEmpty()) {
+    if(square && square.isEmpty()) {
       square.set(color);
+
+      this.turnPieces(square);
+      this.checkWinner();
+
     } else {
-      // throw new Error(`Illegal move. This square already has a piece on (${x}, ${y})`);
+      throw new Error(`Illegal move. This square already has a piece on (${x}, ${y})`);
     }
+  }
+
+  get(x, y) {
+    if(x < this.size && y < this.size) {
+      return this.board[y][x];
+    } else {
+      return new Square();
+    }
+  }
+
+  getAdjacents(square) {
+    const x = square.x;
+    const y = square.y;
+
+    return {
+      top: this.get(x, y - 1),
+      bottom: this.get(x, y + 1),
+      right: this.get(x + 1, y),
+      left: this.get(x - 1, y)
+    };
+  }
+
+  turnPieces(square) {
+    // check adjacent pieces of other color
+    const adjacents = this.getAdjacents(square);
+
+    // check if the adjacent piece is preceded by other piece of different color
+    Object.values(adjacents).forEach((adj) => {
+      if(!adj.isEmpty() && adj.color !== square.color) {
+        if(this.isSurrounded(adj)) {
+          adj.color = square.color;
+        }
+      }
+    });
+  }
+
+  isSurrounded(square) {
+    const adjacents = this.getAdjacents(square);
+    const left = adjacents.left.color;
+    const right = adjacents.right.color;
+    const top = adjacents.top.color;
+    const bottom = adjacents.bottom.color;
+
+    return (left && left === right && square.color !== left) ||
+      (top && top === bottom && square.color !== top);
+  }
+
+  checkWinner() {
+
   }
 
   toString() {
@@ -66,12 +119,14 @@ class Player {
 }
 
 class Square {
-  constructor() {
+  constructor(x, y) {
     this.color = null;
+    this.x = x;
+    this.y = y;
   }
 
   isEmpty() {
-    return this.color === null;
+    return !this.color;
   }
 
   set(color) {
@@ -90,9 +145,13 @@ function main() {
 
   // default play
   p1.play(3, 3);
-  p1.play(4, 4);
   p2.play(3, 4);
+  p1.play(4, 4);
   p2.play(4, 3);
+
+  // Players game
+  p1.play(5, 3);
+  p2.play(3, 2);
 
   console.log(game.board.toString());
 }
