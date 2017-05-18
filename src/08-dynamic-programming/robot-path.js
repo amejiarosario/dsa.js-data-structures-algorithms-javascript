1c271bd5c1e81eb8d34ca5986be591d0f5b19230
Adrian Mejia <admejiar@cisco.com>
2017-05-18 16:08:33 -0400

    8.2 robot grid improvements

--- src/08-dynamic-programming/robot-path.js
+++ src/08-dynamic-programming/robot-path.js
@@ -1,5 +1,11 @@
+let debug = 0;
 /**
  * 8.2 Robot in a Grid: Imagine a robot sitting on the upper left corner of grid with r rows and c columns. The robot can only move in two directions, right and down, but certain cells are "off limits" such that the robot cannot step on them. Design an algorithm to find a path for the robot from the top left to the bottom right.
+ *
+ *
+ * Runtime:
+ *  best case O(r + c)
+ *  worst case O(2^(r + c + o))
  *
  * @param rows
  * @param columns
@@ -9,12 +15,15 @@
  * @returns {*}
  */
 function findRobotPath(rows, columns, obstacle = [], path = [], position = {row: 0, column:0}) {
+  debug++;
   const lastRow = rows - 1;
   const lastColumn = columns - 1;
 
   if(position.row === lastRow && position.column === lastColumn) {
+    // console.log('calls', debug);
     return path;
   } else if(obstacle.some((o) => o.row === position.row && o.column === position.column) || position.row > lastRow || position.column > lastColumn) {
+    // console.log('calls', debug);
     return false;
   } else {
     const moveRight = {row: position.row, column: position.column + 1};
@@ -25,6 +34,46 @@ function findRobotPath(rows, columns, obstacle = [], path = [], position = {row:
 
     return findRobotPath(rows, columns, obstacle, rightPath, moveRight) ||
       findRobotPath(rows, columns, obstacle, downPath, moveDown);
+  }
+}
+
+/**
+ *
+ * This avoids the obstacles before making the call
+ *
+ * @param rows
+ * @param columns
+ * @param obstacle
+ * @param path
+ * @param position
+ * @returns {*}
+ */
+function findRobotPath2(rows, columns, obstacle = [], path = [], position = {row: 0, column:0}) {
+  debug++;
+  const lastRow = rows - 1;
+  const lastColumn = columns - 1;
+
+  if(position.row === lastRow && position.column === lastColumn) {
+    // console.log('calls', debug);
+    return path;
+  } else if(obstacle.some((o) => o.row === position.row && o.column === position.column) || position.row > lastRow || position.column > lastColumn) {
+    // console.log('calls', debug);
+    return false;
+  } else {
+    const moveRight = {row: position.row, column: position.column + 1};
+    const rightPath = path.concat([moveRight]);
+
+    const moveDown = {row: position.row + 1, column: position.column};
+    const downPath = path.concat([moveDown]);
+
+    if(!obstacle.some((o) => o.row === moveRight.row && o.column === moveRight.column) && moveRight.row <= lastRow && moveRight.column <= lastColumn) {
+      return findRobotPath(rows, columns, obstacle, rightPath, moveRight);
+    } else if (!obstacle.some((o) => o.row === moveDown.row && o.column === moveDown.column) && moveDown.row <= lastRow && moveDown.column <= lastColumn) {
+      return findRobotPath(rows, columns, obstacle, downPath, moveDown);
+    } else {
+      // console.log('*calls', debug);
+      return false;
+    }
   }
 }
 
--- src/08-dynamic-programming/robot-path.spec.js
+++ src/08-dynamic-programming/robot-path.spec.js
@@ -16,6 +16,10 @@ describe('findRobotPath', function() {
     ]);
   });
 
+  it('2 x 2 grid and fully blocked obstacles', function () {
+    expect(findRobotPath(2, 2, [{row: 0, column: 1}, {row: 1, column: 0}])).to.eql(false);
+  });
+
   it('4 x 6 grid and no obstacles', function () {
     expect(findRobotPath(4, 6)).to.eql([
       {row: 0, column: 1},
@@ -40,5 +44,10 @@ describe('findRobotPath', function() {
       {row: 4, column: 3},
       {row: 5, column: 3}
     ]);
+  });
+
+  // scale
+  it('3,000 x 3,000 grid and with obstacles', function () {
+    expect(findRobotPath(3e3, 3e3, [{row: 0, column: 1}, {row: 1, column: 2}]).length).to.eql(5998);
   });
 });
