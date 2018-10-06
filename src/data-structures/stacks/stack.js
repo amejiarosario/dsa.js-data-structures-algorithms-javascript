9ca1d02ebb4e0e1f9fd53731dcd5756dc32e0d22
Adrian Mejia <admejiar@cisco.com>
2018-10-06 13:17:36 -0400

    stack docs

--- src/data-structures/stacks/stack.js
+++ src/data-structures/stacks/stack.js
@@ -1,5 +1,5 @@
 const LinkedList = require('../linked-lists/linked-list');
-
+// tag::constructor[]
 /**
  * Data structure that adds and remove elements in a first-in, first-out (FIFO) fashion
  */
@@ -7,26 +7,32 @@ class Stack {
   constructor() {
     this.items = new LinkedList();
   }
+  // end::constructor[]
 
+  // tag::add[]
   /**
-   * Add element into the stack.
-   * Similar to Array.push
+   * Add element into the stack. Similar to Array.push
    * Runtime: O(1)
    * @param {any} item
+   * @returns {stack} instance to allow chaining.
    */
   add(item) {
     this.items.addLast(item);
     return this;
   }
+  // end::add[]
 
+  // tag::remove[]
   /**
    * Remove element from the stack.
    * Similar to Array.pop
    * Runtime: O(1)
+   * @returns {any} removed value.
    */
   remove() {
     return this.items.removeLast();
   }
+  // end::remove[]
 
   /**
    * Size of the queue
@@ -48,4 +54,18 @@ Stack.prototype.push = Stack.prototype.add;
 Stack.prototype.pop = Stack.prototype.remove;
 
 module.exports = Stack;
+
+/* Usage Example:
+// tag::snippet[]
+const stack = new Stack();
+
+stack.add('a');
+stack.add('b');
+stack.remove(); //↪️ b
+stack.add('c');
+stack.remove(); //↪️ c
+stack.remove(); //↪️ a
+// end::snippet[]
+
+//*/
 
