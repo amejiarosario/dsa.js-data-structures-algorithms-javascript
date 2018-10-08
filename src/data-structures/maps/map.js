3e6d495b76f66e7adb4f8e69bd74e24f160b7f0b
Adrian Mejia <admejiar@cisco.com>
2018-10-07 23:34:31 -0400

    hashmap docs

---
+++ src/data-structures/maps/map.js
@@ -0,0 +1,18 @@
+/* JavaScript Built-in Map Usage
+// tag::snippet[]
+const myMap = new Map();
+
+// mapping values to keys
+myMap.set('string', 'foo');
+myMap.set(1, 'bar');
+myMap.set({}, 'baz');
+const obj1 = {};
+myMap.set(obj1, 'test');
+
+// searching values by key
+myMap.get(1); //↪️ bar
+myMap.get('str'); //↪️ foo
+myMap.get({}); //↪️ undefined
+myMap.get(obj1); //↪️ test
+// end::snippet[]
+//*/
