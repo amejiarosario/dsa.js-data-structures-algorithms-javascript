9acbd4bac9849bf5d8b1d98f48a5ff53e6a0151c
Adrian Mejia <me@adrianmejia.com>
2018-10-23 19:02:33 -0400

    better performance

--- src/algorithms/sorting/insertion-sort.js
+++ src/algorithms/sorting/insertion-sort.js
@@ -1,17 +1,14 @@
-const { swap } = require('./sorting-common');
-
 function insertionSort(collection) {
   const array = Array.from(collection);
 
-  for (let outer = 1; outer < array.length; outer += 1) {
+  for (let outer = 0; outer < array.length; outer += 1) {
+    const insert = array[outer];
     let inner = outer - 1;
-    while (inner >= 0 && array[outer] < array[inner]) {
+    while (inner >= 0 && array[inner] > insert) {
+      array[inner + 1] = array[inner];
       inner -= 1;
     }
-    if (outer !== inner + 1) {
-      const [insert] = array.splice(outer, 1);
-      array.splice(inner + 1, 0, insert);
-    }
+    array[inner + 1] = insert;
   }
   return array;
 }
