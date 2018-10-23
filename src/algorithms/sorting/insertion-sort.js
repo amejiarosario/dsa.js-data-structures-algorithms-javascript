631267a78c5fa2cc281d7aa96cc8fc5e495a26f0
Adrian Mejia <me@adrianmejia.com>
2018-10-23 18:39:14 -0400

    refactored insertion sort

--- src/algorithms/sorting/insertion-sort.js
+++ src/algorithms/sorting/insertion-sort.js
@@ -4,12 +4,14 @@ function insertionSort(collection) {
   const array = Array.from(collection);
 
   for (let outer = 1; outer < array.length; outer += 1) {
-    let inner;
-    for (inner = outer - 1;
-      inner >= 0 && array[outer] < array[inner];
-      inner -= 1) {}
-    const [insert] = array.splice(outer, 1);
-    array.splice(inner + 1, 0, insert);
+    let inner = outer - 1;
+    while (inner >= 0 && array[outer] < array[inner]) {
+      inner -= 1;
+    }
+    if (outer !== inner + 1) {
+      const [insert] = array.splice(outer, 1);
+      array.splice(inner + 1, 0, insert);
+    }
   }
   return array;
 }
