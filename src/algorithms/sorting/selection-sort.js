9dc5caf49e2e00da8d1957095152988115f96777
Adrian Mejia <me@adrianmejia.com>
2018-10-24 13:37:42 -0400

    bubble sort

--- src/algorithms/sorting/selection-sort.js
+++ src/algorithms/sorting/selection-sort.js
@@ -2,8 +2,11 @@ const { swap } = require('./sorting-common');
 
 // tag::sort[]
 /**
- * Selection sort
- * @param {Array|Set} collection list to be sorted
+ * Selection sort - start from the first element,
+ * it tries to find any element (to the right) that could be smaller than the current index.
+ * If it finds one, it will swap the positions.
+ * Runtime: O(n^2)
+ * @param {Array|Set} collection elements to be sorted
  */
 function selectionSort(collection) {
   const array = Array.from(collection);
