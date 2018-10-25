f81435e706f6558c24ceb128816be485577f16cb
Adrian Mejia <me@adrianmejia.com>
2018-10-25 12:24:51 -0400

    improve merge sort

--- src/algorithms/sorting/merge-sort.js
+++ src/algorithms/sorting/merge-sort.js
@@ -1,3 +1,9 @@
+// tag::merge[]
+/**
+ * Merge two arrays in ascending order
+ * @param {Array} array1
+ * @param {Array} array2
+ */
 function merge(array1, array2 = []) {
   const mergedLength = array1.length + array2.length;
   const mergedArray = Array(mergedLength);
@@ -6,7 +12,7 @@ function merge(array1, array2 = []) {
     if (i2 >= array2.length || (i1 < array1.length && array1[i1] <= array2[i2])) {
       mergedArray[index] = array1[i1];
       i1 += 1;
-    } else if (i1 >= array1.length || (i2 < array2.length && array2[i2] <= array1[i1])) {
+    } else {
       mergedArray[index] = array2[i2];
       i2 += 1;
     }
@@ -14,14 +20,23 @@ function merge(array1, array2 = []) {
 
   return mergedArray;
 }
+// end::merge[]
 
+// tag::splitSort[]
+/**
+ * Split array in half until two or less elements are left.
+ * Sort these two elements and combine them back using the merge function.
+ * @param {Array} array
+ */
 function splitSort(array) {
   const size = array.length;
+
   if (size < 2) {
     return array;
   } else if (size === 2) {
     return array[0] < array[1] ? array : [array[1], array[0]];
   }
+
   const middle = Math.ceil(size / 2);
 
   return merge(
@@ -29,6 +44,7 @@ function splitSort(array) {
     splitSort(array.slice(middle)),
   );
 }
+// end::splitSort[]
 
 // tag::sort[]
 /**
