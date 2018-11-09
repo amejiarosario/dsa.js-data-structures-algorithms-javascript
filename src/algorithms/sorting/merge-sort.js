2e8c616f4a0d9f2c7853bbc3f60a9771eee1fc53
Adrian Mejia <me@adrianmejia.com>
2018-11-09 17:59:14 -0500

    more sorting docs

--- src/algorithms/sorting/merge-sort.js
+++ src/algorithms/sorting/merge-sort.js
@@ -8,17 +8,17 @@ function merge(array1, array2 = []) {
   const mergedLength = array1.length + array2.length;
   const mergedArray = Array(mergedLength);
 
-  for (let index = 0, i1 = 0, i2 = 0; index < mergedLength; index += 1) {
+  for (let index = 0, i1 = 0, i2 = 0; index < mergedLength; index += 1) { // <1>
     if (i2 >= array2.length || (i1 < array1.length && array1[i1] <= array2[i2])) {
-      mergedArray[index] = array1[i1];
+      mergedArray[index] = array1[i1]; // <2>
       i1 += 1;
     } else {
-      mergedArray[index] = array2[i2];
+      mergedArray[index] = array2[i2]; // <2>
       i2 += 1;
     }
   }
 
-  return mergedArray;
+  return mergedArray; // <3>
 }
 // end::merge[]
 
