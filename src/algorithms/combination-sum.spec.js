bc491d5e4d475097631949f876cc5f6688b32dc4
Adrian Mejia <me@adrianmejia.com>
2019-03-12 09:35:49 -0400

    work with 2 numbers

--- src/algorithms/combination-sum.spec.js
+++ src/algorithms/combination-sum.spec.js
@@ -4,4 +4,23 @@ describe('combinationSum', () => {
   it('should return empty', () => {
     expect(combinationSum([], 0)).toEqual([[]]);
   });
+
+  it('should find solution for one item', () => {
+    expect(combinationSum([1], 1)).toEqual([[1]]);
+  });
+
+  it('should use multiple times one value', () => {
+    expect(combinationSum([1], 2)).toEqual([[1, 1]]);
+  });
+
+  it('should not find solution', () => {
+    expect(combinationSum([2], 1)).toEqual([]);
+  });
+
+  fit('should not find solution', () => {
+    expect(combinationSum([1, 2], 3)).toEqual([
+      [1, 1, 1],
+      [1, 2],
+    ]);
+  });
 });
