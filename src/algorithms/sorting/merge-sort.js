2864380451242a86732602447a336a0b0cc62ce3
Adrian Mejia <me@adrianmejia.com>
2019-03-22 17:46:44 -0400

    adding index terms

--- src/algorithms/sorting/merge-sort.js
+++ src/algorithms/sorting/merge-sort.js
@@ -1,13 +1,19 @@
 // tag::merge[]
 /**
  * Merge two arrays in ascending order
- * @param {Array} array1
- * @param {Array} array2
+ *
+ * @param {array} array1
+ * @param {array} array2
+ * @returns {array} merged arrays in asc order
+ *
+ * @example
+ *    merge([2,5,9], [1,6,7]) => [1, 2, 5, 6, 7, 9]
  */
 function merge(array1, array2 = []) {
   const mergedLength = array1.length + array2.length;
   const mergedArray = Array(mergedLength);
 
+  // merge elements on a and b in asc order. Run-time O(a + b)
   for (let index = 0, i1 = 0, i2 = 0;
     index < mergedLength; index += 1) { // <1>
     if (i2 >= array2.length ||
@@ -29,6 +35,10 @@ function merge(array1, array2 = []) {
  * Split array in half recursively until two or less elements are left.
  * Sort these two elements and combine them back using the merge function.
  * @param {Array} array
+ * @example
+ *    splitSort([3, 2, 1]) => [1, 2, 3]
+ *    splitSort([3]) => [3]
+ *    splitSort([3, 2]) => [2, 3]
  */
 function splitSort(array) {
   const size = array.length;
@@ -36,14 +46,14 @@ function splitSort(array) {
   if (size < 2) {
     return array;
   } else if (size === 2) {
-    return array[0] < array[1] ? array : [array[1], array[0]]; // <2>
+    return array[0] < array[1] ? array : [array[1], array[0]]; // <1>
   }
 
   // recursive split in half and merge back
   const half = Math.ceil(size / 2);
   return merge( // <3>
-    splitSort(array.slice(0, half)), // <1>
-    splitSort(array.slice(half)), // <1>
+    splitSort(array.slice(0, half)), // <2>
+    splitSort(array.slice(half)), // <2>
   );
 }
 // end::splitSort[]
