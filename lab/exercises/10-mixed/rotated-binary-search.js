a3bfa7f2efdffb92bea187252b78deb0577073d7
Adrian Mejia <adriansky@gmail.com>
2020-04-23 08:51:10 -0400

    update exercises

--- lab/exercises/10-mixed/rotated-binary-search.js
+++ lab/exercises/10-mixed/rotated-binary-search.js
@@ -1,24 +1,23 @@
-function search(array, target, l = 0, r = array.length - 1) {
+function search(array, target, lo = 0, hi = array.length - 1) {
   if (!array.length) return -1;
+  if (lo > hi) { return -1; }
 
-  const m = l + Math.floor((r - l) / 2);
+  const m = lo + Math.floor((hi - lo) / 2);
   const M = array[m];
-  const L = array[l];
-  const R = array[r];
+  const L = array[lo];
+  const R = array[hi];
 
   if (M === target) { return m; }
-  if (l > r) { return -1; }
-
   if (
     (R > L && target > M) // sorted array with no rotation
     || (R > M && (target > M && target <= R)) // sorted array with pivot on M or left
     || (M > R && (target > M || target < L)) // sorted array with pivot to right of M
   ) {
     // go right
-    return search(array, target, m + 1, r);
+    return search(array, target, m + 1, hi);
   }
   // go left
-  return search(array, target, l, m - 1);
+  return search(array, target, lo, m - 1);
 }
 
 module.exports = search;
