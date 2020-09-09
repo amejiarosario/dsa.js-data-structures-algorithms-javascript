4209a0655c5db628fcd22b1019c3323169ec5228
Adrian Mejia <adriansky@gmail.com>
2020-09-09 07:41:51 -0400

    --wip-- [skip ci]

---
+++ book/interview-questions/merge-intervals.js
@@ -0,0 +1,27 @@
+// const {  } = require('../../src/index');
+
+// tag::description[]
+function merge(intervals) {
+  // end::description[]
+  // tag::placeholder[]
+  // write your code here...
+  // end::placeholder[]
+  // tag::solution[]
+  const ans = [];
+
+  intervals.sort((a, b) => a[0] - b[0]); // sort by start time
+
+  for (let i = 0; i < intervals.length; i++) {
+    const last = ans[ans.length - 1];
+    const curr = intervals[i];
+    if (last && last[1] >= curr[0]) { // check for overlaps
+      last[1] = Math.max(last[1], curr[1]);
+    } else ans.push(curr);
+  }
+  return ans;
+  // end::solution[]
+  // tag::description[]
+}
+// end::description[]
+
+module.exports = { merge };
