fdb8bd8af644ddeaa07ce62773e9be96fd6b2404
Adrian Mejia <adriansky@gmail.com>
2020-09-11 07:39:03 -0400

    feat(book/sorting): add questions and solutions

---
+++ book/interview-questions/merge-intervals.spec.js
@@ -0,0 +1,30 @@
+const { merge } = require('./merge-intervals');
+// const {  } = require('../../src/index');
+
+[merge].forEach((fn) => {
+  describe(`TOPIC: ${fn.name}`, () => {
+    it('should work with null/empty', () => {
+      const actual = fn([]);
+      const expected = [];
+      expect(actual).toEqual(expected);
+    });
+
+    it('should work with small case', () => {
+      const actual = fn([[1, 3]]);
+      const expected = [[1, 3]];
+      expect(actual).toEqual(expected);
+    });
+
+    it('should work with other case', () => {
+      const actual = fn([[0, 1], [1, 3], [3, 5], [6, 6]]);
+      const expected = [[0, 5], [6, 6]];
+      expect(actual).toEqual(expected);
+    });
+
+    it('should work with other case', () => {
+      const actual = fn([[10, 99], [20, 50], [9, 11], [98, 100]]);
+      const expected = [[9, 100]];
+      expect(actual).toEqual(expected);
+    });
+  });
+});
---
+++ book/interview-questions/sort-colors.js
@@ -0,0 +1,49 @@
+// const {  } = require('../../src/index');
+
+// tag::description[]
+/**
+ * Sort array of 0's, 1's and 2's in linear time and in-place.
+ * @param {numbers[]} nums - Array of number (0, 1, or 2).
+ * @returns {void} Don't return anything, modify the input array.
+ */
+function sortColors(nums) {
+  // end::description[]
+  // tag::placeholder[]
+  // write your code here...
+  // end::placeholder[]
+  // tag::solution[]
+  let left = 0;
+  let right = nums.length - 1;
+  let curr = 0;
+
+  while (curr <= right) {
+    if (nums[curr] < 1) {
+      [nums[curr], nums[left]] = [nums[left], nums[curr]]; // swap
+      left++;
+      curr++;
+    } else if (nums[curr] > 1) {
+      [nums[curr], nums[right]] = [nums[right], nums[curr]]; // swap
+      right--;
+    } else {
+      curr++;
+    }
+  }
+  // end::solution[]
+  // tag::description[]
+}
+// end::description[]
+
+// tag::compact[]
+function sortColorsCompact(nums) {
+  let i = 0, lo = 0, hi = nums.length - 1;
+  const swap = (k, j) => [nums[k], nums[j]] = [nums[j], nums[k]];
+
+  while (i <= hi) {
+    if (nums[i] < 1) swap(i++, lo++);
+    else if (nums[i] > 1) swap(i, hi--);
+    else i++;
+  }
+}
+// end::compact[]
+
+module.exports = { sortColors, sortColorsCompact };
