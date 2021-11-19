67e94eff8a00157cb09d05a56f99766bfbb7258f
Adithya <adithyaofficial99@gmai.com>
2021-11-19 17:30:50 +0530

    Radix sort in javascript

---
+++ src/algorithms/sorting/radix-sort.js
@@ -0,0 +1,19 @@
+function radixSort(arr) {
+    // Find the max number and multiply it by 10 to get a number
+    // with no. of digits of max + 1
+    const maxNum = Math.max(...arr) * 10;
+    let divisor = 10;
+    while (divisor < maxNum) {
+       // Create bucket arrays for each of 0-9
+       let buckets = [...Array(10)].map(() => []);
+       // For each number, get the current significant digit and put it in the respective bucket
+       for (let num of arr) {
+          buckets[Math.floor((num % divisor) / (divisor / 10))].push(num);
+       }
+       // Reconstruct the array by concatinating all sub arrays
+       arr = [].concat.apply([], buckets);
+       // Move to the next significant digit
+       divisor *= 10;
+    }
+    return arr;
+ }
