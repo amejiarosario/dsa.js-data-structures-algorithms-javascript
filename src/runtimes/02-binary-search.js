5b49d8a8a12712328a8de9b94eba949d11553686
Gabriela <gabriela.stelescu@gmail.com>
2022-05-19 14:53:34 +0200

    fix(runtimes/02-binary-search):fixes binary search iterative

--- src/runtimes/02-binary-search.js
+++ src/runtimes/02-binary-search.js
@@ -38,50 +38,24 @@ function binarySearchRecursive(array, search, offset = 0) {
  * @param {string|number} search value to search for
  */
 function binarySearchIterative(array, search) {
-  // console.log('binarySearchIterative', {array, search});
   let start = 0;
-  let end = array.length;
-  const half = () => parseInt((end - start) / 2, 10) + start;
+  let end = array.length - 1;
+  const half = () => start + parseInt((end - start) / 2, 10);
 
-  while (end - start > 0) {
+  while (start <= end) {
     const currentIndex = half();
     const current = array[currentIndex];
 
-    if (current === search) {
-      return currentIndex;
-    } if (search > current) {
-      start = currentIndex;
+    if (current === search) return currentIndex;
+
+    if (search > current) {
+      start = currentIndex + 1;
     } else if (search < current) {
-      end = currentIndex;
+      end = currentIndex - 1;
     }
   }
 
   return -1;
 }
 
-// const binarySearch = binarySearchRecursive;
-const binarySearch = binarySearchIterative;
-
-// function test() {
-//  const directory = ['Adrian', 'Bella', 'Charlotte', 'Daniel',
-//  'Emma', 'Hanna', 'Isabella', 'Jayden', 'Kaylee', 'Luke', 'Mia',
-//  'Nora', 'Olivia', 'Paisley', 'Riley', 'Thomas', 'Wyatt', 'Xander', 'Zoe'];
-//
-//   const assert = require('assert');
-//   assert.equal(binarySearch([], 'not found'), -1);
-//   assert.equal(binarySearch([1], 2), -1);
-//   assert.equal(binarySearch([1], 1), 0);
-//   assert.equal(binarySearch([1, 2, 3], 1), 0);
-//   assert.equal(binarySearch([1, 2, 3], 2), 1);
-//   assert.equal(binarySearch([1, 2, 3], 3), 2);
-//   assert.equal(binarySearch([1, 2, 3], 31), -1);
-//   assert.equal(binarySearch(directory, 'Adrian'), 0);
-//   assert.equal(binarySearch(directory, 'Hanna'), 5);
-//   assert.equal(binarySearch(directory, 'Zoe'), 18);
-//   assert.equal(binarySearch(directory, 'not found'), -1);
-// }
-
-// test();
-
-
-module.exports = { binarySearch, binarySearchIterative, binarySearchRecursive };
+module.exports = { binarySearchIterative, binarySearchRecursive };
--- src/runtimes/02-binary-search.spec.js
+++ src/runtimes/02-binary-search.spec.js
@@ -1,6 +1,6 @@
-const binarySearch = require('./02-binary-search').binarySearchRecursive;
+const { binarySearchRecursive, binarySearchIterative } = require('./02-binary-search');
 
-describe('Binary Search', () => {
+describe('Binary Search Recursive', () => {
   let array;
 
   beforeEach(() => {
@@ -8,22 +8,50 @@ describe('Binary Search', () => {
   });
 
   it('should find a middle element', () => {
-    expect(binarySearch(array, 9)).toEqual(1);
+    expect(binarySearchRecursive(array, 9)).toEqual(1);
   });
 
   it('should find an first element', () => {
-    expect(binarySearch(array, 7)).toEqual(0);
+    expect(binarySearchRecursive(array, 7)).toEqual(0);
   });
 
   it('should find the last element', () => {
-    expect(binarySearch(array, 23)).toEqual(3);
+    expect(binarySearchRecursive(array, 23)).toEqual(3);
   });
 
   it('should not find an bigger element', () => {
-    expect(binarySearch(array, 9000)).toEqual(-1);
+    expect(binarySearchRecursive(array, 9000)).toEqual(-1);
   });
 
   it('should find a smaller element', () => {
-    expect(binarySearch(array, -9)).toEqual(-1);
+    expect(binarySearchRecursive(array, -9)).toEqual(-1);
+  });
+});
+
+describe('Binary Search Iterative', () => {
+  let array;
+
+  beforeEach(() => {
+    array = [7, 9, 13, 23];
+  });
+
+  it('should find a middle element', () => {
+    expect(binarySearchIterative(array, 9)).toEqual(1);
+  });
+
+  it('should find an first element', () => {
+    expect(binarySearchIterative(array, 7)).toEqual(0);
+  });
+
+  it('should find the last element', () => {
+    expect(binarySearchIterative(array, 23)).toEqual(3);
+  });
+
+  it('should not find an bigger element', () => {
+    expect(binarySearchIterative(array, 9000)).toEqual(-1);
+  });
+
+  it('should find a smaller element', () => {
+    expect(binarySearchIterative(array, -9)).toEqual(-1);
   });
 });
